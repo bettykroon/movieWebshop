2b8b60cafc86c293ba6d148a22edbb450a70ee0f
bettykroon <betty.kroon@hotmail.com>
2022-03-08 14:05:26 +0100

    webshop service

--- src/app/services/webshop/webshop.service.ts
+++ src/app/services/webshop/webshop.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { IMovies } from 'src/app/models/IMovies';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebshopService {
+  private webshop = new Subject<IMovies[]>();
+  webshop$ = this.webshop.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(){
+    this.http
+      .get<IMovies[]>( environment.baseUrl + 'products' )
+      .subscribe((data) => {
+        console.log(data);
+        this.webshop.next(data);        
+      });
+  }
 }
