6979392f332efb91b323ac91d9159c550844394d
bettykroon <betty.kroon@hotmail.com>
2022-03-08 13:57:05 +0100

    Skapat upp samtliga filer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ThanksComponent } from './components/thanks/thanks.component';
+import { CartComponent } from './components/cart/cart.component';
+import { WebshopComponent } from './components/webshop/webshop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    CheckoutComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    ThanksComponent,
+    CartComponent,
+    WebshopComponent
   ],
   imports: [
     BrowserModule,
