cb9c779826947aed86cfbc2eff58d28de3344b7e
bettykroon <betty.kroon@hotmail.com>
2022-03-08 14:08:34 +0100

    categories service

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ICategories } from 'src/app/models/ICategories';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
+  private categories = new Subject<ICategories[]>();
+  categories$ = this.categories.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(){
+    this.http
+      .get<ICategories[]>(environment.baseUrl + 'categories')
+      .subscribe((data) => {
+        this.categories.next(data);
+      });
+  }
 }
