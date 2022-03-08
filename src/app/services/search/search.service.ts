db6804803c62bd87212f49d3083e8c294b646cea
bettykroon <betty.kroon@hotmail.com>
2022-03-08 14:07:05 +0100

    search service

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { IMovies } from 'src/app/models/IMovies';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private search = new Subject<IMovies[]>();
+  search$ = this.search.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(movie: string){
+    this.http
+      .get<IMovies[]>(environment.baseUrl + 'search?searchText=' + movie)
+      .subscribe((data) => {
+        console.log(data);
+        this.search.next(data);
+      });
+  }
 }
