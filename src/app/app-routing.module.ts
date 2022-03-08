f82b503ee20f03a731e94baf90130ec7b6b3ac7c
bettykroon <betty.kroon@hotmail.com>
2022-03-08 14:36:28 +0100

    admin component & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ThanksComponent } from './components/thanks/thanks.component';
+import { WebshopComponent } from './components/webshop/webshop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WebshopComponent},
+  {path: 'varukorg', component: CartComponent},
+  {path: 'kassa', component: CheckoutComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'tack', component: ThanksComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IOrder } from 'src/app/models/IOrder';
+import { AdminService } from 'src/app/services/admin/admin.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  admin: IOrder[] = [];
 
-  constructor() { }
+  constructor(private service: AdminService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.service.admin$.subscribe((dataFromApi) => {
+      this.admin = dataFromApi;
+    });
+    this.service.getOrders();
+    console.log("A", this.admin);
+    
   }
 
+  deleteOrder(order: number){
+    console.log(order);
+    this.http
+      .delete<IOrder[]>(environment.baseUrl + 'orders/' + order)
+      .subscribe(() => {
+        location.reload();
+      })
+  }
 }
