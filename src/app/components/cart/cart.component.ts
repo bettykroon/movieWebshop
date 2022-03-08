8a9b94d8f91b57c4c62f39ab51d7c4c82ec2c04c
bettykroon <betty.kroon@hotmail.com>
2022-03-08 14:25:11 +0100

    cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IMovies } from 'src/app/models/IMovies';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  moviesInCart: IMovies[] = [];
+
+  totalprice: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    let moviesInLs = localStorage.getItem('products') || '[]';
+    this.moviesInCart = JSON.parse(moviesInLs);
+
+    let priceInLs = localStorage.getItem('totalPrice') || '[]';
+    this.totalprice = JSON.parse(priceInLs);
   }
 
+  removeFromCart(i: number){
+    this.moviesInCart.splice(i, 1);
+    localStorage.setItem('products', JSON.stringify(this.moviesInCart));
+  }
+
+  removePrice(price: IMovies["price"]){
+    this.totalprice -= price;
+    console.log("totalPris efter ta bort", this.totalprice);
+    localStorage.setItem('totalPrice', JSON.stringify(this.totalprice));
+  }
 }
