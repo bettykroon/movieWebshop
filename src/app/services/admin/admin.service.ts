c308d710e26031d46f9c0013941ecb08c13c5603
bettykroon <betty.kroon@hotmail.com>
2022-03-08 14:09:37 +0100

    admin service

--- src/app/services/admin/admin.service.ts
+++ src/app/services/admin/admin.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { IOrder } from 'src/app/models/IOrder';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  private admin = new Subject<IOrder[]>();
+  admin$ = this.admin.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getOrders(){
+    this.http
+      .get<IOrder[]>(environment.baseUrl + 'orders?companyId=25')
+      .subscribe((data) => {
+        this.admin.next(data);
+        console.log(data);
+      })
+  }
 }
